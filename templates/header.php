17d8d0100b4ac9f896718329d5d46c01399fdf03
tombaro <tombaro@hotmail.com>
2014-12-30 15:41:03 +0100

    Css: Header
    responsive

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,7 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
-	<div class="master-header"></div>
+	<div id="master-header">
+		<span class="inner"></span>
+	</div>
   <div class="container">
 	<div class="navbar-header">
 	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
