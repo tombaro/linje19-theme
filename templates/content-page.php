9fbdb73cb459b88adc0633f9bfa58a7c4f6e7631
tombaro <tombaro@hotmail.com>
2015-01-05 15:50:11 +0100

    Css: Layout for pages

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,3 @@
+<?php if ( ! post_password_required() && ! is_attachment() ) : the_post_thumbnail(); endif; ?>
 <?php the_content(); ?>
 <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
